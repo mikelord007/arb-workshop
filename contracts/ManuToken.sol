302524a9161a7132a77abc8b47bc63aae026b53f
manu jasan <manujasan23@gmail.com>
2024-08-09 12:56:49 +0530

    init

---
+++ contracts/ManuToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Manu is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("Manu", "MNU") Ownable(initialOwner) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
